2bbbdcd9a89a4b49d31ae4d683bfa2376f4d6141
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2024-05-10 13:44:13 -0400

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import "../app.postcss";
+	import { Modal, initializeStores } from "@skeletonlabs/skeleton";
+
+	initializeStores();
+</script>
+
+<Modal regionBackdrop="!bg-black/[.24]" />
+<slot />
